5c2bfd738456c6500e60d41ff5393ca967a9d7a0
YY <395296412@qq.com>
2025-09-15 18:02:10 +0800

    111
    Signed-off-by: YY <395296412@qq.com>

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,12 +4,39 @@
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
+    <custom-button type="primary" @click="handleClick">Primary Button</custom-button>
+    <custom-button type="default" size="mini" plain>Default Mini Plain</custom-button>
+    <custom-button type="warn" disabled>Warn Disabled</custom-button>
+
+    <view class="input-container">
+      <custom-input v-model="inputValue" placeholder="Enter something..."></custom-input>
+      <text>You entered: {{ inputValue }}</text>
+    </view>
   </view>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+<script>
+import CustomButton from '@/components/button.vue';
+import CustomInput from '@/components/input.vue';
+
+export default {
+  components: {
+    CustomButton,
+    CustomInput,
+  },
+  data() {
+    return {
+      title: 'Hello',
+      inputValue: '',
+    };
+  },
+  onLoad() {},
+  methods: {
+    handleClick() {
+      console.log('Button clicked!');
+    },
+  },
+};
 </script>
 
 <style>
@@ -37,5 +64,10 @@ const title = ref('Hello')
 .title {
   font-size: 36rpx;
   color: #8f8f94;
+}
+
+.input-container {
+  margin-top: 20px;
+  width: 80%;
 }
 </style>
